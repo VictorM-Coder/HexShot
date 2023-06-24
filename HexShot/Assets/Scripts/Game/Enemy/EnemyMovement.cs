63f6cf4e17c7db67aa5d199b3b22edcb025c44f5
Herik Cosmo <herikcosmo.m@gmail.com>
2023-06-24 12:05:42 -0300

    sistema basico amaldicoado

--- HexShot/Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ HexShot/Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -1,25 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _rotationSpeed;
+
+    private Rigidbody2D _rigidbody;
+    private PlayerAwarenessController _playerAwarenessController;
+    private Vector2 _targetDirection;
     private SpriteRenderer _spriteRenderer;
-    // Start is called before the first frame update
+    private float _changeDirectionCooldown;
+
 
     private void Awake()
     {
+        _rigidbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-    }
-    void Start()
-    {
-        
+        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+        _targetDirection = transform.up;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection()
+    {
+        HandleRandomDirectionChange();
+        HandlePlayerTargeting();
+    }
+
+    private void HandlePlayerTargeting()
+    {
+        if(_playerAwarenessController.AwareOfPlayer)
+        {
+            _targetDirection = _playerAwarenessController.DirectionToPlayer;
+        }
+    }
+
+    private void HandleRandomDirectionChange()
+    {
+        _changeDirectionCooldown -= Time.deltaTime;
+
+        if(_changeDirectionCooldown <= 0)
+        {
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward); 
+            _targetDirection = rotation * _targetDirection;
+
+            _changeDirectionCooldown = Random.Range(1f, 5f);
+        }
+    }
+
+
+    private void RotateTowardsTarget()
+    {
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+        _rigidbody.SetRotation(rotation);
+    }
+
+    private void SetVelocity()
+    {
+        _rigidbody.velocity = transform.up * _speed;
     }
 
     public void ChangeColor()
