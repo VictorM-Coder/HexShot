6ae8db26a4b022f73a29a7e03b9a3f9e8af7f028
Herik Cosmo <herikcosmo.m@gmail.com>
2023-06-23 21:25:18 -0300

    inimigo basico

--- HexShot/Assets/Scripts/Game/Player/Bullet.cs
+++ HexShot/Assets/Scripts/Game/Player/Bullet.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera _camera;
+
+    private void Awake()
     {
-        
+        _camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DestroyWhenOffScreen();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<EnemyMovement>())
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void DestroyWhenOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if(screenPosition.x < 0 || 
+           screenPosition.x > _camera.pixelWidth ||
+           screenPosition.y < 0 ||
+           screenPosition.y > _camera.pixelHeight)
+        {
+            Destroy(gameObject);
+        }
     }
 }
