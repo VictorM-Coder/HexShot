3a92e0b667509eafcbaedf0d0e968029862fc02a
victor <martinsvictor6781@gmail.com>
2023-07-11 09:32:00 -0300

    musica e ultimos nivel

---
+++ HexShot/Assets/Scripts/Game/Controllers/AudioController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.PlayOneShot(clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!source.isPlaying) {
+            source.PlayOneShot(clip);
+        }
+    }
+}
